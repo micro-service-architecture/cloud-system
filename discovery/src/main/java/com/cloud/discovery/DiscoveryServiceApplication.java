ae0c64b652065d13291fff3f5983cfd00f69d635
haeyonghahn <yong80211@gmail.com>
2022-09-24 16:02:42 +0900

    feat: zuul

--- discovery/src/main/java/com/cloud/discovery/DiscoveryServiceApplication.java
+++ discovery/src/main/java/com/cloud/discovery/DiscoveryServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryserviceApplication {
+public class DiscoveryServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryserviceApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 }
